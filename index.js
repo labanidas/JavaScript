b5913f59dee98f74e7413b119199e446402edaca
Labani Das <labanidas2001@gmail.com>
2022-07-22 18:43:01 +0530

    initial commit

--- index.js
+++ index.js
@@ -1,17 +1,15 @@
-let increase=document.getElementById("increase");
-let reset=document.getElementById("reset");
-let decrease=document.getElementById("decrease");
-let value = document.getElementsByTagName("h1")[0];
-let num = 0;
-increase.onclick = ()=>{
-       num+=1;
-       value.textContent=`${num}`;
-}
-reset.onclick = ()=>{
-    num=0;
-    value.textContent=`${num}`;
-}
-decrease.onclick = ()=>{
-    num-=1;
-    value.textContent=`${num}`;
-}
+const button = document.querySelector("button");
+const userInput = document.querySelector("input");
+const answer = document.querySelector("h2");
+const randomNumber = Math.round(Math.random() * 10 + 1);
+// console.log(randomNumber);
+
+let count = 0;
+
+button.onclick = () => {
+  count++;
+  while (userInput.value != randomNumber) {
+    answer.textContent = "Opps! Wrong guess :)";
+  }
+  answer.textContent = `You made correct guesses after ${count} times`;
+};
