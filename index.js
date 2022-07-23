2b751210be7eb05ee8c3bc435b293e2a80ee5eb8
Labani Das <labanidas2001@gmail.com>
2022-07-23 17:28:37 +0530

    removed while loop instead used if else statement

--- index.js
+++ index.js
@@ -2,14 +2,13 @@ const button = document.querySelector("button");
 const userInput = document.querySelector("input");
 const answer = document.querySelector("h2");
 const randomNumber = Math.round(Math.random() * 10 + 1);
-// console.log(randomNumber);
+console.log(randomNumber);
 
 let count = 0;
 
 button.onclick = () => {
   count++;
-  while (userInput.value != randomNumber) {
+  if (userInput.value != randomNumber) {
     answer.textContent = "Opps! Wrong guess :)";
-  }
-  answer.textContent = `You made correct guesses after ${count} times`;
+  }else  answer.textContent = `You made correct guesses after ${count} times`;
 };
